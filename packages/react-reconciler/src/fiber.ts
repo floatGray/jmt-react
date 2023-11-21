9f18ef59b54ba5af56d781adace14e4350bed79f
floatGray <599316783@qq.com>
2023-11-21 15:42:35 +0800

    feat: 实现reconciler的workloop，一些数据结构的定义

---
+++ packages/react-reconciler/src/fiber.ts
@@ -0,0 +1,91 @@
+import { Props, Key, Ref } from 'shared/ReactTypes';
+import { WorkTag } from './workTags';
+import { Flags, NoFlags } from './fiberFlags';
+import { Container } from 'hostConfig';
+
+export class FiberNode {
+	type: any;
+	tag: WorkTag;
+	pendingProps: Props;
+	key: Key;
+	stateNode: any;
+	ref: Ref;
+
+	return: FiberNode | null;
+	sibling: FiberNode | null;
+	child: FiberNode | null;
+	index: number;
+
+	memoizedProps: Props | null;
+	memoizedState: any;
+	alternate: FiberNode | null;
+	flags: Flags;
+	updateQueue: unknown;
+
+	constructor(tag: WorkTag, pendingProps: Props, key: Key) {
+		// 实例
+		this.tag = tag;
+		this.key = key;
+		// HostComponent <div> div DOM
+		this.stateNode = null;
+		// FunctionComponent () => {}
+		this.type = null;
+
+		// 构成树状结构
+		this.return = null; //指向父node
+		this.sibling = null;
+		this.child = null;
+		this.index = 0; //同级node标记
+
+		this.ref = null;
+
+		// 作为工作单元
+		this.pendingProps = pendingProps; //工作开始前的props
+		this.memoizedProps = null; //工作结束后的props
+		this.memoizedState = null;
+		this.updateQueue = null;
+
+		this.alternate = null; //用于两颗fibernode树的指向
+		// 副作用
+		this.flags = NoFlags;
+	}
+}
+
+export class FiberRootNode {
+	container: Container;
+	current: FiberNode;
+	finishedWork: FiberNode | null;
+	constructor(container: Container, hostRootFiber: FiberNode) {
+		this.container = container;
+		this.current = hostRootFiber;
+		hostRootFiber.stateNode = this;
+		this.finishedWork = null;
+	}
+}
+
+export const createWorkInProgress = (
+	current: FiberNode,
+	pendingProps: Props
+): FiberNode => {
+	let wip = current.alternate;
+
+	if (wip === null) {
+		// mount
+		wip = new FiberNode(current.tag, pendingProps, current.key);
+		wip.stateNode = current.stateNode;
+
+		wip.alternate = current;
+		current.alternate = wip;
+	} else {
+		// update
+		wip.pendingProps = pendingProps;
+		wip.flags = NoFlags;
+	}
+	wip.type = current.type;
+	wip.updateQueue = current.updateQueue;
+	wip.child = current.child;
+	wip.memoizedProps = current.memoizedProps;
+	wip.memoizedState = current.memoizedState;
+
+	return wip;
+};
